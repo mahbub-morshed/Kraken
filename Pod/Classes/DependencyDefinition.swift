cfc97fc5e307f3e7adf1a3ada2575e6d9fe47b6a
Syed Sabir Salman-Al-Musawi <sabirvirtuoso@gmail.com>
2016-04-20 13:55:30 +0600

    Implement FactoryDefinition and perform some refactoring

--- Pod/Classes/DependencyDefinition.swift
+++ Pod/Classes/DependencyDefinition.swift
@@ -6,7 +6,7 @@
 //
 //
 
-public final class DependencyDefinition {
+public final class ImplementationDefinition: DependencyDefinition {
   
   var scope: DependencyScope
   var implementationType: Injectable.Type?
@@ -18,3 +18,26 @@ public final class DependencyDefinition {
     self.implementation = implementation
   }
 }
+
+public final class FactoryDefinition<F>: DependencyDefinition {
+
+  var scope: DependencyScope
+  var factory: F
+  var numberOfArguments: Int
+  var argumentTypes: [Any]?
+
+  init(scope: DependencyScope, factory: F, numberOfArguments: Int = 0, argumentTypes: [Any]? = nil) {
+    self.scope = scope
+    self.factory = factory
+    self.numberOfArguments = numberOfArguments
+    self.argumentTypes = argumentTypes
+  }
+
+  public func supportsAutowiring() -> Bool {
+    let filteredArgumentTypesForAutowiring = argumentTypes?.filter({ $0 is Injectable })
+
+    return numberOfArguments > 0 && filteredArgumentTypesForAutowiring?.count > 0
+  }
+}
+
+public protocol DependencyDefinition: class {}
---
+++ Pod/Classes/RuntimeArguments.swift
@@ -0,0 +1,9 @@
+//
+//  RuntimeArguments.swift
+//  Pods
+//
+//  Created by Syed Sabir Salman on 4/20/16.
+//
+//
+
+import Foundation
